1bc0561fa036125b0a26eba265bc5fafa6bcc0a7
Corentin <shinedownizstr8@gmail.com>
2022-02-08 12:03:11 +0100

    added new component for rank

---
+++ src/Trackmania/PlayerRank.jsx
@@ -0,0 +1,13 @@
+export function PlayerRank(props){
+    const data = props.data;
+    return(
+        <div className="player-rank">
+            <div className="regional-rank">
+                
+            </div>
+            <div className="echelon-type">
+                Echelon
+            </div>
+        </div>
+    )
+}
