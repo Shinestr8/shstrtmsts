a0fbe611fb9568ddc4602f9499510c70190c4523
Corentin <shinedownizstr8@gmail.com>
2022-02-08 15:48:02 +0100

    added data as dependency to useeffect

--- src/Trackmania/TrophyPieChart.jsx
+++ src/Trackmania/TrophyPieChart.jsx
@@ -21,7 +21,7 @@ export function TrophyPieChart(props){
             prevArrayRef.current = props.data;
         }
         
-    });
+    }, [props.data, data]);
     
     const COLORS = ['#003f5c','#2f4b7c','#665191','#a05195','#d45087','#f95d6a','#ff7c43','#ffa600', '#F7CA18'];
     const RADIAN = Math.PI / 180;
