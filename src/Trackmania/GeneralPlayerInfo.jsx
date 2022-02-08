04b32cf76228f44c9ab6168a14781687831dda82
Corentin <shinedownizstr8@gmail.com>
2022-02-08 12:03:00 +0100

    moved regions calculation in parent component

--- src/Trackmania/GeneralPlayerInfo.jsx
+++ src/Trackmania/GeneralPlayerInfo.jsx
@@ -3,23 +3,7 @@ import React from "react";
 export function GeneralPlayerInfo(props){
 
     const data = props.data;
-
-    function formatNumber(number) {
-        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
-
-    function findPlayerRegions(zone){
-        let zoneName = zone.name;
-        let zoneList = [zone]
-        while(zoneName !== 'World'){
-            zone = zone.parent;
-            zoneName = zone.name;
-            zoneList.push(zone)
-        }
-        return(zoneList);
-    }
-
-    let regions = findPlayerRegions(data.trophies.zone);
+    const regions = props.regions
 
 
     
