6cb69263d3be2313d38ccd89f9834d4af135f75c
Corentin <shinedownizstr8@gmail.com>
2022-02-08 13:24:53 +0100

    added the tables with trophie distribution

--- src/Trackmania/TrophyDistribution.jsx
+++ src/Trackmania/TrophyDistribution.jsx
@@ -1,3 +1,45 @@
 export function TrophyDistribution(props){
-    return(<div>Troph distrib</div>)
+    
+    const data = props.data;
+
+
+    function computePercentage(partial, type, total){
+        let percentage = partial*(10**type)/total*100;
+        return parseFloat(percentage).toPrecision(2);
+    }
+
+    function formatNumber(number) {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    return(
+        
+        <div className="trophy-distribution">
+            <table className="trophy-distribution-table">
+                <tbody>
+                    {data.trophies.counts.map(function(count, index){
+                        return(
+                            <tr>
+                                <td>
+                                    <img style={{width: '3rem'}}src={process.env.PUBLIC_URL + '/img/trophies/' + index + '.png'}/>
+                                </td>
+                                <td>Tier {index+1}:</td>
+                                <td>{count}</td>
+                                <td>{computePercentage(count, index, data.trophies.points)}%</td>
+                                <td>{formatNumber(count*(10**index))}</td>
+                            </tr>
+                            
+                        )
+                    })}
+                    <tr>
+                        <td></td>
+                        <td>Total</td>
+                        <td></td>
+                        <td>100%</td>
+                        <td>{formatNumber(data.trophies.points)}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 }
