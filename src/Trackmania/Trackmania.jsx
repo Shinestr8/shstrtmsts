75018fa2c3fb5afb9ac84da22dbfd65af6d564d8
Corentin <shinedownizstr8@gmail.com>
2022-02-08 12:02:36 +0100

    added new section and moved regions calculation to parent element

--- src/Trackmania/Trackmania.jsx
+++ src/Trackmania/Trackmania.jsx
@@ -1,15 +1,29 @@
 import { useState } from "react"
 import { GeneralPlayerInfo } from "./GeneralPlayerInfo";
+import { PlayerRank } from "./PlayerRank";
 import "./trackmania.css";
 
 export function Trackmania(props){
     let [textInput, setTextInput] = useState("");
     let [player, setPlayer] = useState(""); 
     let [data, setData] = useState(null);
+    let [regions, setRegions] = useState(null);
 
     function updateTextInput(e){
         setTextInput(e.target.value);
     }
+
+    function findPlayerRegions(zone){
+        let zoneName = zone.name;
+        let zoneList = [zone]
+        while(zoneName !== 'World'){
+            zone = zone.parent;
+            zoneName = zone.name;
+            zoneList.push(zone)
+        }
+        setRegions (zoneList);
+    }
+
 
     function fetchPlayerInfo(e){
         e.preventDefault();
@@ -20,6 +34,7 @@ export function Trackmania(props){
         })
         .then(function(result){
             setData(result)
+            findPlayerRegions(result.trophies.zone)
         })
         .catch(function(error){
             console.log(error);
@@ -35,13 +50,19 @@ export function Trackmania(props){
                 </button>
             </form>
             fetch info for {player}
-            {data !== null && (
+            {data !== null && regions !== null && (
                 <div>
                     <h1 className="player-name">{data.displayname}</h1>
                     <div className="section">
                         <h2 className="section-title">General</h2>
-                        <GeneralPlayerInfo data={data}/>
+                        <GeneralPlayerInfo data={data} regions={regions}/>
                     </div>
+
+                    <div className="section">
+                        <h2 className="section-title">Rank</h2>
+                        <PlayerRank data={data} regions={regions}/>
+                    </div>
+
                 </div>
             )}
         </div>
