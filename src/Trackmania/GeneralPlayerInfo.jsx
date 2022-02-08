fe11b91d88cd36e09fb555dcc2229cb4da4c0b63
Corentin <shinedownizstr8@gmail.com>
2022-02-08 11:44:06 +0100

    improved generalplayerinfo with flag and classnames

--- src/Trackmania/GeneralPlayerInfo.jsx
+++ src/Trackmania/GeneralPlayerInfo.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export function GeneralPlayerInfo(props){
 
     const data = props.data;
@@ -22,6 +24,23 @@ export function GeneralPlayerInfo(props){
 
     
     return(
-        <div>Country: {regions[regions.length -3].name} Trophies: {data.trophies.points}</div>
+            <div className="general-player-info">
+                
+                <div className="general-country">
+                    <img 
+                        className="general-flag-picture"
+                        alt={"flag-" + regions[regions.length -3].flag} 
+                        src={"https://trackmania.io/img/flags/" + regions[regions.length -3].flag + ".jpg"}
+                    />
+                    <span className="general-country-name">
+                        Country: {regions[regions.length -3].name}
+                    </span>
+                    
+                </div>
+                <div className="general-trophy-count">
+                    Trophies: {data.trophies.points}
+                </div>
+                
+            </div>
     )
 }
