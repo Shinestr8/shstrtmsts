de7f932d27fb83d5742605a7d22bf2ef123faec7
Corentin <shinedownizstr8@gmail.com>
2022-02-08 12:16:17 +0100

    added skeleton for rank

--- src/Trackmania/PlayerRank.jsx
+++ src/Trackmania/PlayerRank.jsx
@@ -1,12 +1,26 @@
 export function PlayerRank(props){
     const data = props.data;
+    const regions = props.regions;
+
     return(
         <div className="player-rank">
             <div className="regional-rank">
-                
+                <table>
+                <tbody>
+                {regions.map(function(region, index){
+                    return(
+                        <tr key={"rank-" + region.name}>
+                            <td>{region.name}</td>
+                            <td style={{paddingLeft: '1rem'}}>{data.trophies.zonepositions[index]}</td>
+                        </tr>
+
+                    );
+                })}
+                </tbody>
+                </table>
             </div>
             <div className="echelon-type">
-                Echelon
+                Echelon {data.trophies.echelon}
             </div>
         </div>
     )
