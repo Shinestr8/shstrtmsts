b3c896631f11e5a3d0067d1e298eb54bd5c34801
Corentin <shinedownizstr8@gmail.com>
2022-02-08 15:42:17 +0100

    added background image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { Trackmania } from './Trackmania/Trackmania';
 
+const header_background = process.env.PUBLIC_URL + '/img/background/header_background.png';
+const page_background = process.env.PUBLIC_URL + '/img/background/page_background.png'
 
 function App() {
   return (
@@ -8,7 +10,7 @@ function App() {
       <div className='header'>
         Trackmania Stats
       </div>
-      <div className='wrapper'>
+      <div className='wrapper' style={{backgroundImage: `url(${page_background})`}}>
         <div className='content'>
           <Trackmania/>
         </div>
