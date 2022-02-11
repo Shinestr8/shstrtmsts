c4e072d9b8f66d36837c112fedecf244c4cb3921
Corentin <shinedownizstr8@gmail.com>
2022-02-11 12:49:54 +0100

    moved most of the Trackmania component in a separate file, to allow having multiple tabs

--- src/Trackmania/Trackmania.jsx
+++ src/Trackmania/Trackmania.jsx
@@ -2,17 +2,15 @@ import { useState } from "react"
 
 import { remoteServer } from "../config";
 
-import { GeneralPlayerInfo } from "./GeneralPlayerInfo";
-import { PlayerRank } from "./PlayerRank";
-import { TrophyDistribution } from "./TrophyDistribution";
-import { PlayerList } from "./PlayerList";
+
+import { GeneralStats } from "./GeneralStats";
 
 import "./trackmania.css";
 import "./responsive.css";
 
 import { UpdateButton } from "../Component/UpdateButton/UpdateButton";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export function Trackmania(){
@@ -173,50 +171,14 @@ export function Trackmania(){
                 )}
             </form>
             <div className="content">
-                    {playerList && (
-                        <div>
-                            <div>No exact match for player <strong>{player}</strong>, is it one of the following ?</div>
-                            <PlayerList data={playerList} onClick={playerSelect}/>
-                        </div>
-                    )}
-
-                    {data && data.message &&(
-                        <div>
-                            <div>{data.message}</div>
-                            <img 
-                                title="marcINSPECT" 
-                                alt='marcINSPECT' 
-                                src={`${process.env.PUBLIC_URL}/gif/marc-inspect-inspect.gif`} 
-                                style={{"width": "20em"}}
-                            />
-                        </div>
-                    )}
-                    {loading &&(
-                        <div className="loading">
-                            <FontAwesomeIcon icon={faSpinner} size="2x"spin/>
-                        </div>
-                    )}
-                    {data && !loading && regions && (
-                        <div>
-                            
-                            <h1 className="player-name">{data.displayname}</h1>
-                            <div className="section">
-                                <h2 className="section-title">General</h2>
-                                <GeneralPlayerInfo data={data} regions={regions}/>
-                            </div>
-
-                            <div className="section">
-                                <h2 className="section-title">Rank</h2>
-                                <PlayerRank data={data} regions={regions}/>
-                            </div>
-
-                            <div className="section">
-                                <h2 className="section-title">Trophy Distribution</h2>
-                                <TrophyDistribution data={data}/>
-                            </div>
-                            
-                        </div>
-                    )}
+                    <GeneralStats
+                        player={player}
+                        data={data}
+                        loading={loading}
+                        playerList={playerList}
+                        regions={regions}
+                        handlePlayerClick={playerSelect}
+                    />
                 </div>
             </div> 
     )
