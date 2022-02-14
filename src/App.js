0d61f7bb880708436f2d52c17c58f328a6d5da76
Corentin <shinedownizstr8@gmail.com>
2022-02-14 11:23:35 +0100

    make background take the entire page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ const page_background = process.env.PUBLIC_URL + '/img/background/page_backgroun
 
 function App() {
   return (
-    <div className="app">
+    <div className="app" style={{backgroundImage: `url(${page_background})`}}>
       <div className='header'>
         Trackmania Stats
       </div>
-      <div className='wrapper' style={{backgroundImage: `url(${page_background})`}}>
+      <div className='wrapper' >
         <div>
           <Trackmania/>
         </div>
