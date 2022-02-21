f8339d04754d189da2f3053fa2f9eb4424c65a91
Corentin <shinedownizstr8@gmail.com>
2022-02-21 15:32:52 +0100

    added a condition on 3v3 and Royal render, if the player never played mm display a message

--- src/Trackmania/Matchmaking/Matchmaking.jsx
+++ src/Trackmania/Matchmaking/Matchmaking.jsx
@@ -4,16 +4,28 @@ import { Royal } from "./Royal";
 export function Matchmaking(props){
     const data = props.data;
 
+    if(!data[0] && !data[1]){
+        return(
+            <div>This user has never played matchmaking, super sorry alexander</div>
+        )
+    }
+
     return(
         <div>
-            <div className="section">
-                <h2 className="section-title">3v3</h2>
-                <MainMM data={data[0]}/>
-            </div>
-            <div className="section">
-                <h2 className="section-title">Royal</h2>
-                <Royal data={data[1]}/>
-            </div>
+            {data[0] && (
+                <div className="section">
+                    <h2 className="section-title">3v3</h2>
+                    <MainMM data={data[0]}/>
+                </div>
+            )}
+            
+            {data[1] && (
+                <div className="section">
+                    <h2 className="section-title">Royal</h2>
+                    <Royal data={data[1]}/>
+                </div>
+            )}
+            
         </div>
     )
 }
