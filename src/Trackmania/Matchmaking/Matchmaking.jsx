ed2d0fbe001b64c623a8e86ee54f6dbd96d2e4d1
Corentin <shinedownizstr8@gmail.com>
2022-02-21 14:41:09 +0100

    added the components

--- src/Trackmania/Matchmaking/Matchmaking.jsx
+++ src/Trackmania/Matchmaking/Matchmaking.jsx
@@ -1,5 +1,19 @@
+import { MainMM } from "./MainMM";
+import { Royal } from "./Royal";
+
 export function Matchmaking(props){
     const data = props.data;
-    
-    return("matchmaking")
+
+    return(
+        <div>
+            <div className="section">
+                <h2 className="section-title">3v3</h2>
+                <MainMM data={data[0]}/>
+            </div>
+            <div className="section">
+                <h2 className="section-title">Royal</h2>
+                <Royal data={data[1]}/>
+            </div>
+        </div>
+    )
 }
