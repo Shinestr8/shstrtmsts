1bd3697678b4c96a0c4ff1c40a54c4ffce6b4ea1
Corentin <shinedownizstr8@gmail.com>
2022-03-03 15:08:06 +0100

    created a theme file for lighttheme and maybe future darktheme

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+export const lightTheme = {
+    background: '#161D31',
+    title: '#FFFFFF',
+    error: '#9C122D',
+    main: '#FFFFFF',
+    font_main: '#000000',
+    menu: '#161D31',
+    menu_hover: '#D3D3D3',
+    menu_selected: '#007BFF',
+    infobox_background: '#153426',
+    infobox_font: '#58CD9F',
+    button:{
+        active_bg: '#007bff',
+        active_color: '#ffffff',
+        disabled_bg: '#cccccc',
+        disabled_color: '#666666'
+    }
+}
