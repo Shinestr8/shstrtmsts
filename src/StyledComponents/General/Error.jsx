0806c4c232c55cdac0c41636c83a185608fcdcac
Corentin <shinedownizstr8@gmail.com>
2022-03-04 11:43:14 +0100

    added styled component for error message

---
+++ src/StyledComponents/General/Error.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ErrorMessage = styled.div`
+    background-color: rgb(156, 18, 45);
+    color: rgba(227, 225, 222, 0.7);
+    padding: 1rem;
+`
