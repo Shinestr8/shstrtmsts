22f314c4a09dadd03dfe75d5de5d95e46a90a02a
Corentin <shinedownizstr8@gmail.com>
2022-03-04 12:31:55 +0100

    added styled component for section and section title

---
+++ src/StyledComponents/General/Section.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Section = styled.div`
+    padding-bottom: 3rem;
+    border-bottom: 1px solid lightgray;
+
+    &:last-of-type{
+        border: none;
+    }
+`
+
+export const SectionTitle = styled.h2`
+    margin: 1rem 0rem 1rem 1rem;
+`
