bda17844f4909ecaa6e883a49fbd91fe77287097
Corentin <shinedownizstr8@gmail.com>
2022-03-07 15:23:34 +0100

    added darktheme and refacto lighttheme

--- src/theme.js
+++ src/theme.js
@@ -1,18 +1,59 @@
-export const lightTheme = {
-    background: '#161D31',
-    title: '#FFFFFF',
-    error: '#9C122D',
-    main: '#FFFFFF',
-    font_main: '#000000',
-    menu: '#161D31',
-    menu_hover: '#D3D3D3',
-    menu_selected: '#007BFF',
-    infobox_background: '#153426',
-    infobox_font: '#58CD9F',
+
+
+export const darkTheme = {
+    background: '#000000',
+    border: '#D3D3D3',
+    font_secondary: '#FFFFFF',
+    font_primary: '#FFFFFF',
+    main: '#161D31',
+    menu:{
+        main: '#000000',
+        hover: '#D3D3D3',
+        selected: '#007BFF',
+        font_light: '#FFFFFF',
+        font_dark: '#000000'
+    },
+    infobox:{
+        background: '#153426',
+        font: '#58CD9F'
+    },
     button:{
         active_bg: '#007bff',
         active_color: '#ffffff',
         disabled_bg: '#cccccc',
         disabled_color: '#666666'
+    },
+    error:{
+        background: '#9C122D',
+        font: '#CDA3A9'
+    }
+}
+
+export const lightTheme = {
+    background: '#161D31',
+    border: '#D3D3D3',
+    font_secondary: '#FFFFFF',
+    font_primary: '#000000',
+    main: '#FFFFFF',
+    menu:{
+        main: '#161D31',
+        hover: '#D3D3D3',
+        selected: '#007BFF',
+        font_light: '#FFFFFF',
+        font_dark: '#000000'
+    },
+    infobox:{
+        background: '#153426',
+        font: '#58CD9F'
+    },
+    button:{
+        active_bg: '#007bff',
+        active_color: '#ffffff',
+        disabled_bg: '#cccccc',
+        disabled_color: '#666666'
+    },
+    error:{
+        background: '#9C122D',
+        font: '#CDA3A9'
     }
 }
