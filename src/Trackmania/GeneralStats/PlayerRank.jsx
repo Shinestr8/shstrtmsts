5cffe5e866d1991fdc5f06e0a950e593aecdf642
Corentin <shinedownizstr8@gmail.com>
2022-03-07 13:27:11 +0100

    use styled component on playerank

--- src/Trackmania/GeneralStats/PlayerRank.jsx
+++ src/Trackmania/GeneralStats/PlayerRank.jsx
@@ -1,45 +1,60 @@
 import { formatRank } from "../../functions/formatRank";
 
+import styled from "styled-components";
+
+const Flag = styled.img`
+    width: 2rem;
+    height: 1rem;
+`
+
+const Echelon = styled.img`
+    max-width: 4rem;
+    max-height: 4rem;
+    @media screen and (max-width: 1024px) {
+        display: none
+    }
+`
+
+const Rank = styled.div`
+    display: flex;
+    justify-content: space-around;
+`
+
 export function PlayerRank(props){
     const data = props.data;
     const regions = props.regions;
 
     return(
-        <div className="player-rank">
-            <div className="regional-rank">
-                <table>
-                    <tbody>
-                        {regions.map(function(region, index){
-                            let imgsrc = index === regions.length - 3 ? process.env.PUBLIC_URL + "/img/flag/4x3/" + region.flag.toLowerCase() + ".svg" : "https://trackmania.io/img/flags/" + region.flag + ".jpg"
-                            return(
-                                <tr key={"rank-" + region.name}>
-                                    <td>
-                                        <img 
-                                            className="rank-flag" 
-                                            title={region.name}
-                                            alt={"flag-" + region.flag} 
-                                            src={imgsrc}
-                                        />
-                                    </td>
-                                    <td>{region.name}</td>
-                                    <td style={{paddingLeft: '1rem'}}>
-                                        {formatRank(data.trophies.zonepositions[index])}
-                                    </td>
-                                </tr>
+        <Rank>
+            <table>
+                <tbody>
+                    {regions.map(function(region, index){
+                        let imgsrc = index === regions.length - 3 ? process.env.PUBLIC_URL + "/img/flag/4x3/" + region.flag.toLowerCase() + ".svg" : "https://trackmania.io/img/flags/" + region.flag + ".jpg"
+                        return(
+                            <tr key={"rank-" + region.name}>
+                                <td>
+                                    <Flag
+                                        title={region.name}
+                                        alt={"flag-" + region.flag} 
+                                        src={imgsrc}
+                                    />
+                                </td>
+                                <td>{region.name}</td>
+                                <td style={{paddingLeft: '1rem'}}>
+                                    {formatRank(data.trophies.zonepositions[index])}
+                                </td>
+                            </tr>
 
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
-            <div className="echelon-type">
-                <img 
-                    className="player-rank-echelon"
-                    title={"echelon-" + data.trophies.echelon}
-                    alt={"echelon-" + data.trophies.echelon}
-                    src={process.env.PUBLIC_URL + '/img/echelon/' + data.trophies.echelon + '.png'}
-                />
-            </div>
-        </div>
+                        );
+                    })}
+                </tbody>
+            </table>
+            
+            <Echelon         
+                title={"echelon-" + data.trophies.echelon}
+                alt={"echelon-" + data.trophies.echelon}
+                src={process.env.PUBLIC_URL + '/img/echelon/' + data.trophies.echelon + '.png'}
+            />
+        </Rank>
     )
 }
