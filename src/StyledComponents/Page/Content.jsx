9b0f870f749bd1ee4d4b07a2396b2530e559479e
Corentin <shinedownizstr8@gmail.com>
2022-03-07 12:57:33 +0100

    fixed margin 0 on content when screen is smaller

--- src/StyledComponents/Page/Content.jsx
+++ src/StyledComponents/Page/Content.jsx
@@ -4,4 +4,8 @@ export const Content = styled.div`
     background-color: ${(props) => props.theme.main};
     border-radius: 7px;
     margin: 0 1rem 1rem 1rem;
+
+    @media screen and (max-width: 1024px){
+        margin: 0;
+    }
 `
