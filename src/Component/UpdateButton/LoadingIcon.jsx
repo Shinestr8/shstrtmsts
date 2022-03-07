582885994dde06d99092e7a43e3f6a30628ee51d
Corentin <shinedownizstr8@gmail.com>
2022-03-07 13:47:24 +0100

    refacto loading icon with styled

--- src/Component/UpdateButton/LoadingIcon.jsx
+++ src/Component/UpdateButton/LoadingIcon.jsx
@@ -1,10 +1,19 @@
 import {faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import styled from "styled-components"
+
+
+const StyledDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 3rem;
+`
+
 export function LoadingIcon(){
     return(
-        <div className="loading">
+        <StyledDiv>
             <FontAwesomeIcon icon={faSpinner} size="2x"spin/>
-        </div>
+        </StyledDiv>
     )
 }
