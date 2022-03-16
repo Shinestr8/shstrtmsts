15cb2a73acf7332dc77ad4127974029e9e3ab4a0
Corentin <shinedownizstr8@gmail.com>
2022-03-16 15:50:25 +0100

    complete themeswitch refacto for accessibility: toggle is now controlable with keyboard alt + space

--- src/StyledComponents/Input/ThemeSwitch.jsx
+++ src/StyledComponents/Input/ThemeSwitch.jsx
@@ -1,38 +1,7 @@
-//external
+// //external
 import styled from "styled-components";
 import {faMoon } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-const StyledDiv = styled.div`
-    display:flex;
-    align-self: flex-end;
-    padding: 1rem;
-`
-
-const StyledSwitch = styled.div`
-    background-color: ${(props) => props.checked ? props.theme.button.active_bg: props.theme.button.disabled_bg};
-    width: 3.2rem;
-    height: 2rem;
-    border-radius: 34px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    padding-left: 0.1rem;
-    padding-right: 0.1rem;
-    cursor: pointer;
-    transition-property: background-color;
-    transition-duration: 0.3s;
-`
-
-const Round = styled.div`
-    border-radius: 50%;
-    background-color: #ffffff;
-    width: 1.5rem;
-    height: 1.5rem;
-    margin-left: ${(props) => props.checked ? "1.6rem" : "0rem"};
-    transition-property: margin;
-    transition-duration:0.3s;   
-`
 
 const StyledIcon = styled(FontAwesomeIcon)`
     margin-left: 0.5rem;
@@ -42,20 +11,74 @@ const StyledIcon = styled(FontAwesomeIcon)`
     }
 `
 
+const StyledDiv = styled.div`
+    display:flex;
+    align-self: flex-end;
+    padding: 1rem;
+`
+
+const StyledLabel = styled.label`
+    position: relative;
+    display: inline-block;
+    width: 3.2rem;
+    height: 2rem;
+`
+
+const Checkbox = styled.input`
+    position: absolute;
+    opacity: 0;
+    pointer-events: none;
+    &:checked +.switch::before {
+        transform: translateX(1.5rem);
+    }
+    &:focus+.switch  {
+        outline: #5d9dd5 solid 1px;
+        box-shadow: 0 0 8px #5e9ed6;
+    }
+    &:checked +.switch{
+        background-color: ${(props)=>props.theme.button.active_bg};
+    }
+`
+
+const Switch = styled.span`
+    position: absolute;
+    cursor: pointer;
+    background-color: ${(props)=>props.theme.button.disabled_bg};
+    border-radius: 25px;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    transition: background-color 0.2s ease;
+    
+    &&:before{
+        position: absolute;
+        content: "";
+        left: 0.1rem;
+        top: 0.25rem;
+        width: 1.5rem;
+        height: 1.5rem;
+        background-color: #fff;
+        border-radius: 50%;
+        transition: transform 0.3s ease;
+    }
+`
+
 
 export function ThemeSwitch(props){
-
-    function handleClick(){
+    
+    function handleChange(){
         props.handleClick();
     }
 
     return(
         <StyledDiv>
-            <StyledSwitch onClick={handleClick} checked={props.checked}>
-                <Round checked={props.checked}>
-                </Round>
-            </StyledSwitch>
+            <StyledLabel className="toggle-switch">
+                <Checkbox type="checkbox" checked={props.checked} onChange={handleChange} />
+                <Switch className="switch" />
+            </StyledLabel>
             <StyledIcon checked={props.checked} icon={faMoon} size="2x"/>
         </StyledDiv>
     )
+
 }
